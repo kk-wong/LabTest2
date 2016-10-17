01e7ccaa58bac669534adff729913d967640b1ca
Kenneth Wong <kennethwong@wifi-staff-172-24-47-245.net.auckland.ac.nz>
2016-10-17 17:42:03 +1300

    Final practice Test

--- PracticeLabTest2/Assets/Sketch.cs
+++ PracticeLabTest2/Assets/Sketch.cs
@@ -6,15 +6,25 @@ public class Sketch : MonoBehaviour {
 	public Material material1;  
 	public Material material2; 
 	public Material material3; 
+	public Material material4;
+	public GameObject cube;
 
 
-
-
+	//Variables for dataset
+	private string readingID;
+	private string location;
+	private string safetyCategory;
+	private string safetyMeasure;
+	private string whenReadingRecorded;
+	private float x;
+	private float y;
+	private float z;
+	public string jsonResponse;
 	public string _WebsiteURL = "http://kwon709.azurewebsites.net/tables/WaterPollutionReading?zumo-api-version=2.0.0";
 
 	void Start () {
 
-		string jsonResponse = Request.GET(_WebsiteURL);
+		jsonResponse = Request.GET(_WebsiteURL);
 
 		//Just in case something went wrong with the request we check the reponse and exit if there is no response.
 
@@ -27,59 +37,83 @@ public class Sketch : MonoBehaviour {
 		// convert the table into an array (this is shown in deserialise) 
 		WaterPollutionReading[] waterpollutionreadings = JsonReader.Deserialize<WaterPollutionReading[]>(jsonResponse);
 
-		int totalCubes = waterpollutionreadings.Length;
-		int totalDistance = 5;
-		int i = 0;
+
 		foreach (WaterPollutionReading waterpollutionreading in waterpollutionreadings)
 		{
-			Debug.Log("This person passed away at the age of: " + waterpollutionreading.SafetyCategory);
-			float perc = i / (float)totalCubes;
-			i++;
-			float x = 0.0f;
 
+			Debug.Log("X is " + waterpollutionreading.X + ", Y is " + waterpollutionreading.Y + ", Z is " + waterpollutionreading.Z + "" ) ;
+			readingID = waterpollutionreading.ReadingID;
+			location = waterpollutionreading.Location;
+			safetyCategory = waterpollutionreading.SafetyCategory;
+			safetyMeasure = waterpollutionreading.SafetyMeasure;
+			whenReadingRecorded = waterpollutionreading.WhenReadingRecorded;
+			x = float.Parse(waterpollutionreading.X);
+			y = float.Parse(waterpollutionreading.Y);
+			z = float.Parse(waterpollutionreading.Z);
+
+			GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+			newCube.name = waterpollutionreading.ReadingID;			
+			//adds label based on axis- can change this to the safety category or other
+			newCube.GetComponentInChildren<TextMesh> ().text= "(" + waterpollutionreading.X + ", " + waterpollutionreading.Y + ", " + waterpollutionreading.Z + ")";
+			//changes color
 			if (waterpollutionreading.SafetyCategory == "High") {
-				// float y = where it is on the y axis- where is it rotating on the y axis- You have three different levels and this is changed by float y
-				float y = 0.25f;
-				float z = 16.0f;
-				// Create a new cube for every object in that table
-				GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-				// <shows what you can put in- refer to unity>().field it is
-				// changes cube size based on script in mycubescript- you can change this value directly
-				//setsize(method) vs setsize = field
-				//newCube.GetComponent<mySphereScript>().setSize(0.3f);
-				//newCube.GetComponent<mySphereScript>().rotateSpeed = perc;
-				newCube.GetComponentInChildren<TextMesh> ().text= waterpollutionreading.SafetyCategory;
-
-				//Colors attribute that you can make 
 				newCube.GetComponent<Renderer> ().material = material1;
 
-			} 
-
-			else if (waterpollutionreading.SafetyCategory == "Medium") {
-				float y = 0.25f;
-				float z = 17.0f;
-				GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-				//newCube.GetComponent<mySphereScript>().setSize(0.5f);
-				//newCube.GetComponent<mySphereScript>().rotateSpeed = perc;
-				newCube.GetComponentInChildren<TextMesh> ().text = waterpollutionreading.SafetyCategory;
-			//	newCube.GetComponent<mySphereScript>().rotateAxis = Vector3.down;
+			} else if (waterpollutionreading.SafetyCategory == "Medium") {
 				newCube.GetComponent<Renderer> ().material = material2;
 
-			} 
-			else {
-				float y = 0.25f;
-				float z = 17.0f;
-				GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-			//	newCube.GetComponent<mySphereScript>().setSize(0.7f);
-			//	newCube.GetComponent<mySphereScript>().rotateSpeed = perc;
-				newCube.GetComponentInChildren<TextMesh> ().text = waterpollutionreading.SafetyCategory;
-				newCube.GetComponent<Renderer> ().material = material3 ;
+			} else {
+
+				newCube.GetComponent<Renderer> ().material = material3;
+
 			}
+
+		}
+
+
+	}
+
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit= new RaycastHit();
+			//whereever your mouse is you get that position
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			//if you hit the object (or with mouse)
+			if (Physics.Raycast (ray, out hit, 100)) {
+
+				//hit your - you get spehre and you can do what you want with it- i.e. change its colour
+				hit.collider.gameObject.GetComponent<Renderer> ().material = material4;
+				Debug.Log (hit.collider.name);
+
+				//
+				int index = int.Parse(hit.collider.gameObject.name);
+
+				WaterPollutionReading[] waterpollutionreadings = JsonReader.Deserialize<WaterPollutionReading[]>(jsonResponse);
+
+				//creating a cube where you hit the raycast
+				//ReadingID is 1- so you add -1 otherwise you will get index out of bound
+				GameObject newCube = (GameObject)Instantiate(cube, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+				newCube.GetComponentInChildren<TextMesh> ().text= "(" + waterpollutionreadings[index-1].Location + ", " + waterpollutionreadings[index-1].Y + ", " + waterpollutionreadings[index-1].Z + ")";
+			}
+
 		}
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
 }
+
+//Update is called once per frame    
+
+
+//if (waterpollutionreading.X) {
+// float y = where it is on the y axis- where is it rotating on the y axis- You have three different levels and this is changed by float y
+//float y = 0.55f;
+//float z = 16.0f;
+// Create a new cube for every object in that table
+//GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+// <shows what you can put in- refer to unity>().field it is
+// changes cube size based on script in mycubescript- you can change this value directly
+//setsize(method) vs setsize = field
+//newCube.GetComponent<mySphereScript>().setSize(0.3f);
+//newCube.GetComponent<mySphereScript>().rotateSpeed = perc;
--- PracticeLabTest2/Assets/mySphereScript.cs
+++ PracticeLabTest2/Assets/mySphereScript.cs
@@ -3,18 +3,41 @@ using System.Collections;
 
 public class mySphereScript : MonoBehaviour {
 
-	//public float rotateSpeed = 1.0f;
-	//public Vector3 spinSpeed = Vector3.zero;
-	//Vector3 spinAxis = new Vector3(0, 1, 0);
-	//public Vector3 rotateAxis = Vector3.up;
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinSpeed = Vector3.zero;
+	Vector3 spinAxis = new Vector3(0, 1, 0);
+	public Vector3 rotateAxis = Vector3.up;
 	public Material material4;
-
 	// Use this for initialization
-	//void Start () {
-	//	spinSpeed = new Vector3(Random.value, Random.value, Random.value);
-	//	spinAxis = Vector3.up;
-	//	spinAxis.x = (Random.value - Random.value)*0.1f;
+	void Start () {
+		spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+		spinAxis = Vector3.up;
+		spinAxis.x = (Random.value - Random.value)*0.1f;
+	}
+
+
+	//void OnMouseDown() {
+		//change color on click
+		//this.GetComponent<Renderer> ().material = material4;
 	//}
+		//changes size on click
+		//this.transform.localScale = new Vector3 (1, 1, 1);
+	
+	//void OnMouseUp () {
+			//Destroy(GameObject);
+
+
+	//}
+
+	//void OnMouseUp () {
+		// changes sizes when mouse not on click
+		//this.transform.localScale = new Vector3 (1, 1, 1);
+	//}
+	//use this" because its inside the script already. 
+	//this.rotateSpeed = 1.0f;
+	// this is also a part of a vector. 
+	//this.spinSpeed = Vector3.one;
+	//this.spinAxis = Vector3.one;
 
 	//public void setSize(float size)
 	//{
@@ -23,22 +46,11 @@ public class mySphereScript : MonoBehaviour {
 
 	// Update is called once per frame- change it to what we have changed. That is why they all have diff speeds.
 	//void Update () {
-		//this.transform.Rotate(spinSpeed);
-		// You give it a variable that you can change around- 
-		//this.transform.RotateAround(Vector3.zero, rotateAxis, rotateSpeed);
+	//this.transform.Rotate(spinSpeed);
+	// You give it a variable that you can change around- 
+	//this.transform.RotateAround(Vector3.zero, rotateAxis, rotateSpeed);
 
 	//}
 
 	// click on one thing and it stops rotating. 
-
-	void OnMouseDown() {
-		//use this" because its inside the script already. 
-		//this.rotateSpeed = 0.0f;
-		// this is also a part of a vector. 
-		//this.spinSpeed = Vector3.zero;
-		//change color on click
-		this.GetComponent<Renderer>().material = material4;
-
-
-	}
 }
