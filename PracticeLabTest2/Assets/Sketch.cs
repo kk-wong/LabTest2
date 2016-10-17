45d09db343a98f7c43617e47b0d058e063911c02
Kenneth Wong <kennethwong@wifi-staff-172-24-47-245.net.auckland.ac.nz>
2016-10-17 18:45:16 +1300

    Final Lab Test 2

--- PracticeLabTest2/Assets/Sketch.cs
+++ PracticeLabTest2/Assets/Sketch.cs
@@ -11,16 +11,16 @@ public class Sketch : MonoBehaviour {
 
 
 	//Variables for dataset
-	private string readingID;
+	private string treeID;
 	private string location;
-	private string safetyCategory;
-	private string safetyMeasure;
+	private string ecologicalvalue;
+	private string historicalsignificance;
 	private string whenReadingRecorded;
 	private float x;
 	private float y;
 	private float z;
 	public string jsonResponse;
-	public string _WebsiteURL = "http://kwon709.azurewebsites.net/tables/WaterPollutionReading?zumo-api-version=2.0.0";
+	public string _WebsiteURL = "http://kwon709.azurewebsites.net/tables/TreeSurvey?zumo-api-version=2.0.0";
 
 	void Start () {
 
@@ -35,31 +35,31 @@ public class Sketch : MonoBehaviour {
 
 		//We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
 		// convert the table into an array (this is shown in deserialise) 
-		WaterPollutionReading[] waterpollutionreadings = JsonReader.Deserialize<WaterPollutionReading[]>(jsonResponse);
+		TreeSurvey[] treesurveys = JsonReader.Deserialize<TreeSurvey[]>(jsonResponse);
 
 
-		foreach (WaterPollutionReading waterpollutionreading in waterpollutionreadings)
+		foreach (TreeSurvey treesurvey in treesurveys)
 		{
 
-			Debug.Log("X is " + waterpollutionreading.X + ", Y is " + waterpollutionreading.Y + ", Z is " + waterpollutionreading.Z + "" ) ;
-			readingID = waterpollutionreading.ReadingID;
-			location = waterpollutionreading.Location;
-			safetyCategory = waterpollutionreading.SafetyCategory;
-			safetyMeasure = waterpollutionreading.SafetyMeasure;
-			whenReadingRecorded = waterpollutionreading.WhenReadingRecorded;
-			x = float.Parse(waterpollutionreading.X);
-			y = float.Parse(waterpollutionreading.Y);
-			z = float.Parse(waterpollutionreading.Z);
+			Debug.Log("X is " + treesurvey.X + ", Y is " + treesurvey.Y + ", Z is " + treesurvey.Z + "" ) ;
+			treeID = treesurvey.TreeID;
+			location = treesurvey.Location;
+			ecologicalvalue = treesurvey.EcologicalValue;
+			historicalsignificance = treesurvey.HistoricalSignificance;
+			whenReadingRecorded = treesurvey.WhenReadingRecorded;
+			x = float.Parse(treesurvey.X);
+			y = float.Parse(treesurvey.Y);
+			z = float.Parse(treesurvey.Z);
 
 			GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-			newCube.name = waterpollutionreading.ReadingID;			
+			newCube.name = treesurvey.TreeID;			
 			//adds label based on axis- can change this to the safety category or other
-			newCube.GetComponentInChildren<TextMesh> ().text= "(" + waterpollutionreading.X + ", " + waterpollutionreading.Y + ", " + waterpollutionreading.Z + ")";
+			newCube.GetComponentInChildren<TextMesh> ().text= "(Location: " + treesurvey.Location + ")";
 			//changes color
-			if (waterpollutionreading.SafetyCategory == "High") {
+			if (treesurvey.Location == "Albert Park") {
 				newCube.GetComponent<Renderer> ().material = material1;
 
-			} else if (waterpollutionreading.SafetyCategory == "Medium") {
+			} else if (treesurvey.Location == "Myers Park") {
 				newCube.GetComponent<Renderer> ().material = material2;
 
 			} else {
@@ -90,12 +90,12 @@ public class Sketch : MonoBehaviour {
 				//
 				int index = int.Parse(hit.collider.gameObject.name);
 
-				WaterPollutionReading[] waterpollutionreadings = JsonReader.Deserialize<WaterPollutionReading[]>(jsonResponse);
+				TreeSurvey[] treesurveys = JsonReader.Deserialize<TreeSurvey[]>(jsonResponse);
 
 				//creating a cube where you hit the raycast
 				//ReadingID is 1- so you add -1 otherwise you will get index out of bound
 				GameObject newCube = (GameObject)Instantiate(cube, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
-				newCube.GetComponentInChildren<TextMesh> ().text= "(" + waterpollutionreadings[index-1].Location + ", " + waterpollutionreadings[index-1].Y + ", " + waterpollutionreadings[index-1].Z + ")";
+				newCube.GetComponentInChildren<TextMesh> ().text= "(Location:" + treesurveys[index-1].Location + "Ecological Value: " + treesurveys[index-1].EcologicalValue + ", Historical Significance: " + treesurveys[index-1].HistoricalSignificance + ", When Reading Recorded" + treesurveys[index-1].WhenReadingRecorded + ")";
 			}
 
 		}
--- PracticeLabTest2/Assets/TreeSurvey.cs
+++ PracticeLabTest2/Assets/TreeSurvey.cs
@@ -4,20 +4,16 @@ using System.Linq;
 using System.Text;
 
 //Make sure you get rid of the namespace that is automatically generated here.
-class WaterPollutionReading
+class TreeSurvey
 {
 	//List all the columns of your table here in the same format as below.
-	public string id { get; set; }
-	public string createdAt { get; set; }
-	public string updatedAt { get; set; }
-	public string version { get; set; }
-	public string ReadingID { get; set; }
+	public string TreeID { get; set; }
 	public string WhenReadingRecorded { get; set; }
 	public string Location { get; set; }
-	public string SafetyCategory { get; set; }
-	public string SafetyMeasure { get; set; }
+	public string HistoricalSignificance { get; set; }
+	public string EcologicalValue { get; set; }
 	public string X { get; set; }
 	public string Y { get; set; }
 	public string Z { get; set; }
-	public string deleted { get; set; }
+
 }
